efe7098324efa4eedd386af118a4c8343e6d2c65
krunal <krunalparekh29.ddu@gmail.com>
2024-03-07 20:01:03 +0530

    services done

---
+++ ATMCLIENTApp/ATMCLIENTApp/CHANGEPIN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ATMCLIENTApp
+{
+    public partial class CHANGEPIN : Form
+    {
+        public CHANGEPIN()
+        {
+            InitializeComponent();
+        }
+    }
+}
